f27bd93fbc80158c5f4760d600c11e1da1f646c2
Joe Warwick <jrwarwick@hotmail.com>
2020-10-19 18:25:06 +1100

    Add test

---
+++ OctopusDeployTest/Tests/ReleaseProcessorTests.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OctopusDeployTest.Tests
+{
+    [TestClass]
+    public class ReleaseProcessorTests
+    {
+
+        [TestMethod]
+        public void TestJsonRead()
+        {
+
+        }
+    }
+}
